c570555780ced849833c11274e657a467648b20d
notrlyrainy <69054170+notrlyrainy@users.noreply.github.com>
2020-07-31 13:49:48 -0500

    broke stuff

--- TeamGame2/Assets/BrokenScript.cs
+++ TeamGame2/Assets/BrokenScript.cs
@@ -7,7 +7,7 @@ public class BrokenScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("anything");
     }
 
     // Update is called once per frame
