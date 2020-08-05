3b3641c080143a2e096573709ff1724b16958369
notrlyrainy <69054170+notrlyrainy@users.noreply.github.com>
2020-08-05 13:27:22 -0500

    built map

---
+++ TeamGame2/Assets/enemyfollow.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.SceneManagement;
+
+public class enemyfollow : MonoBehaviour
+{
+    NavMeshAgent eAgent;
+    GameObject Player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        eAgent = GetComponent<NavMeshAgent>();
+        Player = GameObject.FindGameObjectWithTag("Player");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        eAgent.SetDestination(Player.transform.position);
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player")) {
+            //end game
+            SceneManager.LoadScene("Menu Scene");
+        }
+
+    }
+}
