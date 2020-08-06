b3cfa4b07456faa804e991e1b49244eae0d12f03
lucyliu7 <24liul@flintridgeprep.org>
2020-08-06 10:55:51 -0700

    pause menu

---
+++ TeamGame2/Assets/Scripts/PauseMenuButton.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuButton : MonoBehaviour
+{
+    bool isPaused = false;
+    public GameObject PauseMenu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            TogglePause();
+        }
+    }
+    void TogglePause()
+    {
+        if (isPaused)
+        {
+            ResumeGame();
+        }
+        else
+        {
+            PauseGame();
+        }
+    }
+    public void ResumeGame()
+    {
+        isPaused = false;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        Time.timeScale = 1.0f;
+        PauseMenu.SetActive(false);
+    }
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    public void QuitGame()
+    {
+        SceneManager.LoadScene("Menu Scene");
+    }
+    public void PauseGame()
+    {
+        isPaused = true;
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.Confined;
+        Time.timeScale = 0.0f;
+        PauseMenu.SetActive(true);
+    }
+}
