6d6ef5856ab9a2775a69afd9714e9ff88aeb8dae
notrlyrainy <69054170+notrlyrainy@users.noreply.github.com>
2020-08-06 13:11:51 -0500

    Changed Lighting

--- TeamGame2/Assets/Scripts/MainMenuButtons.cs
+++ TeamGame2/Assets/Scripts/MainMenuButtons.cs
@@ -8,6 +8,7 @@ public class MainMenuButtons : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Cursor.lockState = CursorLockMode.Confined;
         
     }
 
